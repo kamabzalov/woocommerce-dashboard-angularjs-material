5fa1fdd898657ea2b05672fce64dde4fa673fa8d
Kamil Abzalov <mail@kamil-abzalov.ru>
2020-01-19 22:54:42 +0300

    refactoring

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ require('../wp-load.php');
 $error = null;
 session_start();
 
-if (!empty($_POST)) {
-    $creds = array();
-    $creds['user_login'] = strip_tags(trim($_POST['login']));
-    $creds['user_password'] = strip_tags(trim($_POST['password']));
-    $user = wp_signon($creds, false);
+if(!empty($_POST)) {
+	$creds = array();
+	$creds['user_login'] = strip_tags(trim($_POST['login']));
+	$creds['user_password'] = strip_tags(trim($_POST['password']));
+	$user = wp_signon($creds, false);
 
-    if ($user->errors) {
-        $error = 'Неверный логин или пароль';
-
-    } else {
-        $_SESSION['user'] = $creds['user_login'];
-        header('Location: manager');
-        $error = null;
-    }
+	if($user->errors) {
+		$error = 'Неверный логин или пароль';
+	
+	} else {
+		$_SESSION['user'] = $creds['user_login'];
+		header('Location: manager');
+		$error = null;
+	}
 }
 
 ?>
@@ -25,38 +25,38 @@ if (!empty($_POST)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.css">
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.css">
 </head>
 <body>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <form method="post">
-                <fieldset class="mb-2">
-                    <legend>Вход</legend>
-                    <div class="form-group">
-                        <label for="login">Логин</label>
-                        <input type="text" class="form-control" id="login" name="login" placeholder="Логин">
-                    </div>
-                    <div class="form-group">
-                        <label for="login">Пароль</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Пароль">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Вход</button>
-                </fieldset>
-                <?php if ($error) { ?>
-                    <div class="alert alert-dismissible alert-danger">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        <?php echo $error; ?>
-                    </div>
-                <?php } ?>
-            </form>
-        </div>
-    </div>
-</div>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<form method="post">
+					<fieldset class="mb-2">
+						<legend>Вход</legend>
+						<div class="form-group">
+							<label for="login">Логин</label>
+							<input type="text" class="form-control" id="login" name="login" placeholder="Логин">
+						</div>
+						<div class="form-group">
+							<label for="login">Пароль</label>
+							<input type="password" class="form-control" id="password" name="password" placeholder="Пароль">
+						</div>
+						<button type="submit" class="btn btn-primary">Вход</button>
+					</fieldset>
+					<?php if ($error) { ?>
+						<div class="alert alert-dismissible alert-danger">
+							<button type="button" class="close" data-dismiss="alert">&times;</button>
+							<?php echo $error; ?>
+						</div>
+					<?php } ?>
+				</form>
+			</div>
+		</div>
+	</div>
+	
 </body>
 </html>
--- manager/js/script.js
+++ manager/js/script.js
@@ -1,119 +1,119 @@
 'use strict'
 
-// TODO: add another statuses
 let statuses = new Map();
-statuses.set("processing", "в обработке");
-
+statuses.set('processing', 'в обработке');
+		
 let app = angular.module('shop', ['ngMaterial', 'ngMessages', 'ngRoute']);
 app.constant('API_URL', 'https://codetogether.ru/wp-json/wc/v3/');
 app.constant('STATUSES', statuses);
 app.constant('DUMMY_IMAGE', 'https://via.placeholder.com/460/460');
 
-app.config(function ($httpProvider) {
-    let auth = window.btoa('ck_000e1f9b7fd5200992a3fdb46f982c495ac721f8:cs_93ff2e592ee90b6c54d45abe78277b0992684979');
-    $httpProvider.defaults.headers.common.Authorization = 'Basic ' + auth;
+app.config(function($httpProvider){
+	let auth = window.btoa('ck_000e1f9b7fd5200992a3fdb46f982c495ac721f8:cs_93ff2e592ee90b6c54d45abe78277b0992684979');
+	$httpProvider.defaults.headers.common.Authorization = 'Basic ' + auth;
 });
 
-app.config(function ($mdThemingProvider, $routeProvider, $locationProvider) {
-    $mdThemingProvider.theme('primary').primaryPalette('blue').accentPalette('orange');
-    $routeProvider
-        .when(
-            '/products', {
-                templateUrl: 'templates/products.html',
-                controller: 'productsController'
-            }
-        )
-        .when(
-            '/product', {
-                templateUrl: 'templates/product.html',
-                controller: 'productController'
-            }
-        )
-        .when(
-            '/product/:id', {
-                templateUrl: 'templates/product.html',
-                controller: 'productController'
-            }
-        )
-        .otherwise({
-            templateUrl: 'templates/orders.html',
-            controller: 'ordersController'
-        })
-    $locationProvider.html5Mode({
-        enabled: true
-    });
+app.config(function($mdThemingProvider, $routeProvider, $locationProvider){
+	$mdThemingProvider.theme('primary').primaryPalette('blue').accentPalette('orange');
+	$routeProvider
+		.when(
+			'/products', {
+				templateUrl: 'templates/products.html',
+			    controller: 'productsController'
+			}
+		)
+		.when(
+			'/product', {
+				templateUrl: 'templates/product.html',
+				controller: 'productController'
+			}
+		)
+		.when(
+			'/product/:id', {
+				templateUrl: 'templates/product.html',
+				controller: 'productController'
+			}
+		)
+		.otherwise({
+			templateUrl: 'templates/orders.html',
+			controller: 'ordersController'
+        }) 
+		$locationProvider.html5Mode({
+			enabled: true
+		});
 });
 
-app.filter("orderStatus", function (STATUSES) {
-    return function (status) {
-        if (STATUSES.get(status)) {
-            return STATUSES.get(status);
-        } else {
-            return '';
-        }
-    }
+app.filter('ordersStatus', function(STATUSES){
+	return function(status) {
+		if (STATUSES.get(status)) {
+			return STATUSES.get(status);
+		} else {
+			return '';
+		}
+	}
 });
 
-app.filter("removeTags", function () {
-    return function (string) {
-        return string ? String(string).replace(/<[^>]+>/gm, '') : '';
-    }
+app.filter('removeTags', function(){
+	return function(string) {
+		return string ? String(string).replace(/<[^>]+>/gm, '')  : '';
+	}
 })
 
-app.controller('mainController', function ($scope, $http, $location, API_URL) {
-    $scope.currentNavItem = $location.path().split('/')[1];
+app.controller('mainController', function($scope, $location) {
+	$scope.currentNavItem = $location.path().split('/')[1];
 });
 
-app.controller('ordersController', function ($scope, $http, API_URL, $mdDialog) {
-    $scope.pageName = 'Заказы';
-    $scope.hideProgress = false;
+app.controller('ordersController', function ($scope, $http, $mdDialog, API_URL) {
+	$scope.pageName = 'Заказы';
+	$scope.orders = [];
+	$scope.hideProgress = false;
+	
+	$scope.getOrders = function() {
+		$http.get(API_URL + 'orders').then(
+			result => {
+				if(result.data.length) {
+					$scope.orders = result.data
+				} else {
+					$scope.hideProgress = true;
+				}
+			}
+		);
+	}
 
-    $scope.getOrders = function () {
-        $http.get(API_URL + 'orders').then(
-            result => {
-                if (result.data.length) {
-                    $scope.orders = result.data
-                } else {
-                    $scope.hideProgress = true;
-                }
-            },
-            _ => {
-                $scope.hideProgress = true;
-                $mdDialog.show(
-                    $mdDialog.alert()
-                        .clickOutsideToClose(true)
-                        .title('Ошибка получения списка заказов')
-                        .ok('Закрыть')
-                );
-            }
-        );
-    }
-
-    $scope.deleteOrder = function (orderId) {
-        $http.delete(API_URL + `orders/${orderId}`).then(
-            result => $scope.getOrders(),
-            _ => $mdDialog.show(
-                $mdDialog.alert()
-                    .clickOutsideToClose(true)
-                    .title('Ошибка удаления заказа')
-                    .ok('Закрыть')
-            )
-        );
-    }
+	$scope.deleteOrder = function(orderId) {
+		$http.delete(API_URL + `orders/${orderId}`).then(
+				result => {
+					$scope.getOrders();
+					$mdDialog.show(
+						$mdDialog.alert()
+						.clickOutsideToClose(true)
+						.title('Ошибка удаления заказа')
+						.ok('Закрыть')
+					);
+				},
+				_ => $mdDialog.show(
+						$mdDialog.alert()
+						.clickOutsideToClose(true)
+						.title('Ошибка удаления заказа')
+						.ok('Закрыть')
+					)
+			)
+	};
 });
 
-app.controller('productsController', function ($scope, $http, API_URL, DUMMY_IMAGE, $mdDialog) {
-    $scope.pageName = 'Товары';
-    $scope.hideProgress = false;
-    $scope.dummyImage = DUMMY_IMAGE;
-    $scope.page = 0;
-    $scope.disabled = false;
+app.controller('productsController', function ($scope, $http, $mdDialog, API_URL, DUMMY_IMAGE) {
+
+	$scope.pageName = 'Товары';
+	$scope.hideProgress = false;
+	$scope.dummyImage = DUMMY_IMAGE;
+	$scope.page = 0;
+	$scope.disabled = false;
+	$scope.products = [];
 
     $scope.getProducts = function () {
         $scope.page++;
         $http.get(API_URL + `products?page=${$scope.page}`).then(
             result => {
-                    $scope.products = [];
                 if (result.data.length > 0) {
                     result.data.forEach(item => $scope.products.push(item));
                     $scope.hideProgress = true;
@@ -122,94 +122,93 @@ app.controller('productsController', function ($scope, $http, API_URL, DUMMY_IMA
                 }
             }
         );
-    };
-
-    $scope.deleteProduct = function (id) {
-        $http.delete(API_URL + `products/${id}`).then(
-            result => $scope.getProducts(),
-            _ => $mdDialog.show(
-                $mdDialog.alert()
-                    .clickOutsideToClose(true)
-                    .title('Ошибка удаления товара')
-                    .ok('Закрыть')
-            )
-        );
-    }
+	};
+	
+	$scope.deleteProduct = function(id) {
+		$http.delete(API_URL + `products/${id}`).then(
+			_ => $scope.getProducts(),
+			_ => $mdDialog.show(
+						$mdDialog.alert()
+						.clickOutsideToClose(true)
+						.title('Ошибка удаления заказа')
+						.ok('Закрыть')
+					)
+			)
+	}
 });
 
-app.controller('productController', function ($scope, $http, $routeParams, API_URL, $mdDialog, $filter, $location) {
-    $scope.productName = '';
-    $scope.productPrice = '';
-    $scope.productDescription = '';
-    $scope.productCategories = [];
-    $scope.categories = [];
-    if ($routeParams.id) {
-        let productId = +$routeParams.id;
-        $http.get(API_URL + `products/${productId}`).then(
-            product => {
-                $scope.productName = product.data.name;
-                $scope.productPrice = product.data.price;
-                $scope.productDescription = $filter('removeTags')(product.data.description);
-                product.data.categories.forEach(category => {
-                    $scope.productCategories.push(category.id);
-                })
-            },
-            error => {
-                $mdDialog.show(
-                    $mdDialog.alert()
-                        .clickOutsideToClose(true)
-                        .title('Ошибка. Товар не найден')
-                        .ok('Закрыть')
-                );
-            }
-        )
-    }
+app.controller('productController', function ($scope, $http, $routeParams, $mdDialog, $filter, $location, API_URL) {
+	$scope.productName = '';
+	$scope.productPrice = '';
+	$scope.productDesc = '';
+	$scope.productCats = [];
+	$scope.categories = [];
+	$scope.productId = -1;
 
-    $scope.addProduct = function () {
-        const categories = [];
-        if (!$scope.productCategories.length) {
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .clickOutsideToClose(true)
-                    .title('Ошибка добавления товара')
-                    .ok('Закрыть')
-            );
-            return;
-        }
-        $scope.productCategories.forEach(id => {
-            categories.push({'id': id});
-        });
-        const newProduct = {
-            name: $scope.productName,
-            type: 'simple',
-            regular_price: $scope.productPrice,
-            description: $scope.productDescription,
-            short_description: $scope.productDescription,
-            categories: categories
-        }
-        $http.post(API_URL + 'products', newProduct).then(
-            res => {
-                if (res) {
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                            .clickOutsideToClose(true)
-                            .title('Успех')
-                            .ok('Закрыть')
-                    );
-                    $location.url('/products');
-                }
-            }
-        )
-    }
+	if($routeParams.id) {
+		$scope.productId = $routeParams.id;
+		$http.get(API_URL + `products/${$scope.productId}`).then(
+			product => {
+				$scope.productName = product.data.name;
+				$scope.productPrice = product.data.price;
+				$scope.productDesc = $filter('removeTags')(product.data.description);
+				product.data.categories.forEach(category => {
+					$scope.productCats.push(category.id);
+				})
+			}
+		)
+	}
 
-    $scope.getCategories = function () {
-        $http.get(API_URL + 'products/categories').then(
-            res => {
-                if (res.data.length) {
-                    $scope.categories = res.data;
-                }
-            }
-        )
-    }
+	$scope.saveProduct = function() {
+		const categories = [];
+		const newProduct = {
+			name: $scope.productName,
+			type: 'simple',
+			regular_price: $scope.productPrice,
+			description: $scope.productDesc,
+			short_description: $scope.productDesc,
+			categories: categories
+		};
+		if($scope.productId == -1) {
+			if(!$scope.productCats.length) {
+				$mdDialog.show(
+					$mdDialog.alert()
+					.clickOutsideToClose(true)
+					.title('Ошибка сохранения товара')
+					.ok('Закрыть')
+				);
+				return;
+			}
+			$scope.productCats.forEach(id => {
+				categories.push({'id': id});
+			});
+			$http.post(API_URL + 'products', newProduct).then(
+				res => {
+					if (res) {
+						$mdDialog.show(
+							$mdDialog.alert()
+							.clickOutsideToClose(true)
+							.title('Успех')
+							.ok('Закрыть')
+						);
+						$location.url('/products');
+					}
+				}
+			)			
+		} else {
+			$http.put(API_URL + `products/${$scope.productId}`, newProduct).then(
+				res => $location.url('/products')
+			);
+		}
 
+	}
+
+	$scope.getCategories = function() {
+		$http.get(API_URL + `products/categories`).then(
+			res => {
+				if (res.data.length) {
+					$scope.categories = res.data;
+				}
+			}
+		)}
 });
